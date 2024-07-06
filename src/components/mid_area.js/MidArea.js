d0167462d2dd30e562790790d8a4ed2053f4cdd5
Shiva Tripathi <122081782+shivatripathi100@users.noreply.github.com>
2024-07-06 19:17:39 +0530

    Update MidArea.js

--- src/components/mid_area.js/MidArea.js
+++ src/components/mid_area.js/MidArea.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 import "./style.css";
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { spriteActions } from "../../store/sprite-slice";
-import { selectedSpriteActions } from "../../store/selected_sprite_slice";
-import { spriteStyleActions } from "../../store/sprite_style_slice";
+// import { useDispatch } from "react-redux";
+// import { spriteActions } from "../../store/sprite-slice";
+// import { selectedSpriteActions } from "../../store/selected_sprite_slice";
+// import { spriteStyleActions } from "../../store/sprite_style_slice";
 
 export default function MidArea() {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
   const selectedSpriteId = useSelector(state => state.selectedStripe.stripeId)
   const allStripes = useSelector(state => state.sprite.sprites)
   const [currentSpriteActions, setSpriteActions] = useState([])
